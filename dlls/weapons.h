0b10f62d355b5dbca4c9951d2bc26f1d11e989a2
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-04-03 17:29:26 -0400

    Fixed glock ammo values.

--- dlls/weapons.h
+++ dlls/weapons.h
@@ -124,7 +124,7 @@ public:
 #define WEAPON_NOCLIP			-1
 
 //#define CROWBAR_MAX_CLIP		WEAPON_NOCLIP
-#define GLOCK_MAX_CLIP			17
+#define GLOCK_MAX_CLIP			15
 #define PYTHON_MAX_CLIP			6
 #define MP5_MAX_CLIP			50
 #define MP5_DEFAULT_AMMO		25
@@ -141,7 +141,7 @@ public:
 
 
 // the default amount of ammo that comes with each gun when it spawns
-#define GLOCK_DEFAULT_GIVE			17
+#define GLOCK_DEFAULT_GIVE			30
 #define PYTHON_DEFAULT_GIVE			6
 #define MP5_DEFAULT_GIVE			25
 #define MP5_DEFAULT_AMMO			25
